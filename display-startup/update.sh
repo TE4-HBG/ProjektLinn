5ccd8e07afc76de94980cf01e791f1a5a72ed820
KLohger <fanimdude32@gmail.com>
2023-01-10 15:56:49 +0100

    changed ordering of file

--- display-startup/update.sh
+++ display-startup/update.sh
@@ -1,10 +1,10 @@
-
 sudo apt update
+
+sudo apt -y upgrade
 
 sudo apt -y install xorg
 sudo apt -y install firefox-esr
 sudo apt -y install xdotool
-sudo apt -y install xbanish
-sudo apt -y upgrade
+
 git fetch
 git pull --ff-only
