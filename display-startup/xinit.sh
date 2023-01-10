506c61beb0f3e70c2e2b7015d47fc4566cc8a37a
KLohger <fanimdude32@gmail.com>
2023-01-10 15:51:39 +0100

    xinit.sh actually waits for X to fully start now

--- display-startup/xinit.sh
+++ display-startup/xinit.sh
@@ -2,47 +2,59 @@
 
 secret=paprika
 secretLen=7
-website=te4projekt.se/display
 
-printf "\033[38;2;255;255;0mWAITING FOR SECRET KEY:\033[39m\n"
+website=infotavla.te4projekt.se/display
 
+function wait_for_X {
+	while true
+	do 
+		if ! timeout 1s xset q &>/dev/null; then
+			printf "x still dead lol\n"
+		else
+			break
+		fi
+		sleep 1
+	done
+}
+
+printf "\033[38;2;255;255;0mWAITING FOR SECRET KEY:\033[39m "
 read -t 5 -n "$secretLen"; RETVAL=$?
 printf "\n"
-# ok? echo && continue
+
+# if i got a reply and its equal to $secret, then
 if [ $RETVAL -eq 0 ] && [ "$REPLY" = "$secret" ]
 then
 	printf "\033[38;2;0;255;0mDEBUG ENABLED!\033[39m\n"
 else
 	printf "\033[38;2;255;0;0mRUNNING X\033[39m\n";
 	# runs X in background
-	X &
+	X -nocursor &
 	# defines display for other commands
 	export DISPLAY=:0.0;
-	# wait a while until X finished loading (bruh)
-	sleep 5
+	# wait for X to start up
+	wait_for_X
 
 	# hide the mouse
-	./xtoggle.cprog &
+	#./xtoggle.cprog &
 
 
 	# launch firefox in background
-	DISPLAY=:0.0 /usr/bin/firefox -kiosk -private-window "$website" &
-	# wait a while until firefox finished loading
-	sleep 5
-	# change screen orientation
-	xrandr --orientation left
+	/usr/bin/firefox -kiosk -private-window "$website" &
+	
 	while true
 	do
-	# WAIT A WHILE
-	sleep 5
+		sleep 1
+		
+		size=$(xrandr | grep '*' | awk '{print $1}')
+		width=$(printf "$size" | awk '{split($1,arr,"x"); print arr[1]}')
+		height=$(printf "$size" | awk '{split($1,arr,"x"); print arr[2]}')
 
-	size=$(xrandr | grep '*' | awk '{print $1}')
-	width=$(printf "$size" | awk '{split($1,arr,"x"); print arr[1]}')
-	height=$(printf "$size" | awk '{split($1,arr,"x"); print arr[2]}')
-
-	# set firefox size and width to screen
-	xdotool search "Mozilla Firefox" windowmove 0 0
-	xdotool search "Mozilla Firefox" windowsize "$height" "$width"
+		# change screen orientation
+		xrandr --orientation left
+		
+		# set firefox size and width to screen
+		xdotool search "Mozilla Firefox" windowmove 0 0
+		xdotool search "Mozilla Firefox" windowsize "$height" "$width"
 	done
 fi
 
