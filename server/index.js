de6f5b98a01939163271de5d2a5d0e21875df05e
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2023-01-17 16:25:22 +0100

    test: tt
    Co-Authored-By: AnnaSverrePanna <83768759+AnnaSverrePanna@users.noreply.github.com>
    Co-Authored-By: IsaacHog <111504782+IsaacHog@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -87,6 +87,7 @@ let displayInfo = { templates: [] };
 
     function CheckForNonTemplateChanges(templates) {
         for (let i = 0; i < templates.length; i++) {
+            console.log(JSON.parse(templates[i].foodSchedules))
             if (templates[i].duration === null && JSON.parse(templates[i].foodSchedules) != null) {
                 // Ah! Theres been changes to the food schedule!
                 console.log("Detected changes to food schedule")
