bcc407e3cd0bf938bd635cf1028fc2307641104c
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2023-01-17 15:33:08 +0100

    test: teeeeest (foodSchedule)
    Co-Authored-By: AnnaSverrePanna <83768759+AnnaSverrePanna@users.noreply.github.com>
    Co-Authored-By: IsaacHog <111504782+IsaacHog@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -95,8 +95,9 @@ let displayInfo = { templates: [] };
                 // Check if the new data already exists in savedSchedule
                 for (let x = 0; x < savedSchedule.length; x++) {
                     for (let y = 0; y < templates[i].foodSchedule.length; y++) {
-                        console.log(templates[i].foodSchedule[y].week);
-                        console.log(savedSchedule[x].week);
+                        console.log(templates[i]);
+                        console.log(templates[i].foodSchedule[y]);
+                        console.log(savedSchedule[x]);
                         if (savedSchedule[x].week === templates[i].foodSchedule[y].week) {
                             // If the new week already exists in savedSchedule, replace it with new week.
                             savedSchedule[x] = templates[i].foodSchedule[y];
