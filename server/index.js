f462b2371708f7ce4d50a1ba33e331cb3df4ecca
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2023-01-17 15:44:50 +0100

    test: test
    Co-Authored-By: AnnaSverrePanna <83768759+AnnaSverrePanna@users.noreply.github.com>
    Co-Authored-By: IsaacHog <111504782+IsaacHog@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -86,7 +86,6 @@ let displayInfo = { templates: [] };
     }
 
     function CheckForNonTemplateChanges(templates) {
-        console.log(templates.length);
         for (let i = 0; i < templates.length; i++) {
             if (templates[i].duration === null && templates[i].foodSchedule != null) {
                 // Ah! Theres been changes to the food schedule!
@@ -94,6 +93,7 @@ let displayInfo = { templates: [] };
                 // Load foodSchedule file to array of objects
                 const savedSchedule = JSON.parse(fs.readFileSync('foodSchedule.txt', 'utf8'));
                 // Check if the new data already exists in savedSchedule
+                console.log(savedSchedule.length);
                 for (let x = 0; x < savedSchedule.length; x++) {
                     for (let y = 0; y < templates[i].foodSchedule.length; y++) {
                         //console.log(templates[i]);
