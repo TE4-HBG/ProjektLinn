74ebe981910eb48025255aa2fce2542b917e23d1
KLohger <fanimdude32@gmail.com>
2023-01-18 12:33:09 +0100

    docs: fixed spelling error

--- server/index.js
+++ server/index.js
@@ -79,7 +79,7 @@ let displayInfo = { templates: [], skåneTrafiken: null };
 
         // If client closes connection, stop sending events
         res.on('close', () => {
-            console.log(`${new Date().toISOString()}: ${req.ip} disconnecting to event site.`);
+            console.log(`${new Date().toISOString()}: ${req.ip} disconnected from event site.`);
             clearInterval(interValID);
             res.end();
         });
