60c9ebc8083b93d4ddcc665f34ec8dec4d239ffa
KLohger <fanimdude32@gmail.com>
2023-01-19 13:46:21 +0100

    varfär

--- server/index.js
+++ server/index.js
@@ -37,11 +37,13 @@ let displayInfo = { templates: [], skåneTrafiken: { trainData: [], busData: [] 
 (async () => {
     displayInfo.skåneTrafiken = await SkåneTrafiken.Get();
     console.log("skåne be new")
+    console.log(displayInfo.skåneTrafiken);
 })()
 
 setInterval(async () => {
     displayInfo.skåneTrafiken = await SkåneTrafiken.Get();
     console.log("skåne be new")
+    console.log(displayInfo.skåneTrafiken);
 }, 30000);
 
 
