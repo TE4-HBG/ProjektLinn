f0df5105472622b9b5317d0f8dc1ec360a2a8adc
Jakob Helgesson <jakob.helgesson@gmail.com>
2024-01-12 10:44:59 +0100

    Introduce tanstack query

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import { rootRoute } from './routes/index.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
+import { rootRoute } from "./routes/index.jsx";
 
-const router = createBrowserRouter([
-  rootRoute
-])
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([rootRoute]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </React.StrictMode>
+);
