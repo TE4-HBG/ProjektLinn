d18ad3be639f378c38bc09266e0bcbcb61dddc47
Jakob Helgesson <jakob.helgesson@gmail.com>
2024-01-19 10:47:03 +0100

    Introduce Display page (#36)
    * Co-authored-by: Rami-Agha <Rami-Agha@users.noreply.github.com>
    Co-authored-by: Kirill Puljavin <KirillPuljavin@users.noreply.github.com>
    
    * Display Container Core
    
    * Co-authored-by: Rami-Agha <Rami-Agha@users.noreply.github.com>
    
    * update
    
    * Auto stash before checking out "origin/main"
    
    * Add libs
    
    * Co-authored-by: Rami-Agha <Rami-Agha@users.noreply.github.com>
    Co-authored-by: Jakob Helgesson <jakob.helgesson@gmail.com>
    
    * Slides edit
    
    * admin page update
    
    Co-authored-by: Ebbsteer <Ebbsteer@users.noreply.github.com>
    
    * admin update 2
    
    * Header footer start
    
    * Update web/src/components/DisplayHeaderFooter.jsx
    
    * Update web/src/components/DisplayContainer.jsx
    
    ---------
    
    Co-authored-by: Jonte88 <jonathan.thj15@gmail.com>
    Co-authored-by: SFG <kirill.puljavin@gmail.com>
    Co-authored-by: Ebbsteer <Ebbsteer@users.noreply.github.com>
    Co-authored-by: Ebbsteer <Ebba.elversson@gmail.com>

--- web/src/components/DisplayContainer.jsx
+++ web/src/components/DisplayContainer.jsx
@@ -1,15 +1,23 @@
+import { DisplayFooter } from "./DisplayHeaderFooter";
+import { DisplayHeader } from "./DisplayHeaderFooter";
+
 export const DisplayContainer = () => {
   return (
     <>
-      <p className="container p-10 text-center"> Display Container </p>
+      <DisplayHeader />
+      <p className="container p-10 text-center bg-white "> Display Container </p>
+      <DisplayFooter />
     </>
   );
 };
 
+
 export const EditContainer = () => {
   return (
     <>
-      <p className="container p-10 text-center"> Edit Container </p>
+      <div className="container mx-auto px-4 border-4 w-[30vw]">
+        <p className="container p-10 text-center"> Edit Container </p>
+      </div>
     </>
   );
 };
