37c4003e06e5e19e0c28230f5fc2ce6664d75203
Othman Al-Ani <142592326+Othman-Al-Ani@users.noreply.github.com>
2024-01-25 09:50:39 +0100

    InstagramWidget (#45)
    * Create Insta widget component
    
    * Ready for departure
    
    * Remove package lock
    
    ---------
    
    Co-authored-by: Othmam-Al-Ani <142592326+Othmam-Al-Ani@users.noreply.github.com>

---
+++ web/src/components/Insta.jsx
@@ -0,0 +1,22 @@
+import Iframe from 'react-iframe'
+
+
+export const Insta = () => {
+    return (
+   
+           //Iframe to show instagram profile
+        // not completelty tested BE CAREFUL MIGHT EXPLODE
+            
+            <Iframe url="https://www.instagram.com/ntihelsingborg//embed"
+               width="100%"
+               height="100%"
+               id=""
+               className=""
+               display="block"
+               position="relative"
+               scrolling='no'
+               />
+      
+            )
+            }
+            
